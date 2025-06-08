5687d932c98fcb3d5c9d2a6fe536819cf4b61423
KPLAY <utd888@gmail.com>
2025-06-08 20:02:09 +0700

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,22 +1,10 @@
-// firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAtxMJQMwzhZz9QtiHuBlF4L58jRKgI90A",
   authDomain: "highthand-f9bc6.firebaseapp.com",
   projectId: "highthand-f9bc6",
-  storageBucket: "highthand-f9bc6.appspot.com",
+  storageBucket: "highthand-f9bc6.firebasestorage.app",
   messagingSenderId: "60442905889",
   appId: "1:60442905889:web:6d1f9f25a03e0a6a5dc260",
   measurementId: "G-G2CN0QMEJB"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { app, auth, db, storage };
